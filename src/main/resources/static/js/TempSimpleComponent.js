b0ed1d74cad03916e3e5fdd2a105c39c35e61987
StanislavLapitsky <slapitsky@gmail.com>
2017-07-06 10:36:28 +0300

    Adding react

---
+++ src/main/resources/static/js/TempSimpleComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+  render() {
+    return (
+      <div>
+      SimpleComponent rendered 5
+      </div>
+    );
+  }
+}
+
+export default SimpleComponent;
